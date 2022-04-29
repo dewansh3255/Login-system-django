53788ff257d289d48796323b6d1db0e1c4b0b183
dewansh khandelwal <dewansh3255@gmail.com>
2022-04-30 02:29:08 +0530

    some updates

--- signup/info.py
+++ signup/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'dewanshkai19@svvv.edu.in'
-EMAIL_HOST_PASSWORD =  'ASD@rupesh2'
+EMAIL_HOST_USER = 'authentication.dk@gmail.com'
+EMAIL_HOST_PASSWORD ='Dewansh123'
 EMAIL_PORT = 587
